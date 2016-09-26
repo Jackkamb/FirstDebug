90bffb201231a1a8ee71cee694c85754b451452b
Jackkamb <17kambja@wcsu.net>
2016-09-25 20:12:17 -0400

    Update MovingDisk.java

--- MovingDisk.java
+++ MovingDisk.java
@@ -7,14 +7,13 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class MovingDisk extands JPanel
-                  implements ActionListener
+public class MovingDisk extends JPanel implements ActionListener
 {
   private int time;
-
+  
   public MovingDisk()
   {
-    time = 0
+    time = 0;
     Timer clock = new Timer(30, this);
     clock.start;
   }
@@ -22,15 +21,22 @@ public class MovingDisk extands JPanel
   public void paintComponent(Graphics g)
   {
     int x = 150 - (int)(100 * Math.cos(0.005 * Math.PI * time));
-    int y = 130 - (int)75 * Math.sin(0.005 * Math.PI * time));
+    int y = 130 - (int)(75 * Math.sin(0.005 * Math.PI * time));
     int r = 20;
-
     Color sky;
-    if (y > 130) sky = Color.BLACK
-    else sky = Color.CYAN;
+    
+    if (y > 130)
+     {
+     sky = Color.BLACK;
+     }
+     
+    else
+     {
+    sky = Color.CYAN;
     setBackground(sky);
     super.paintComponent(g);
-
+     }
+     
     g.setColor(Color.ORANGE);
     g.fillOval(x - r, y - r, 2*r, 2*r);
   }
@@ -43,15 +49,12 @@ public class MovingDisk extands JPanel
 
   public static void main(String args)
   {
-    JFrame w = new JFrame("Moving Disk);
+    JFrame w = new JFrame(MovingDisk);
     w.setSize(300, 150);
-
     Container c = w.getContentPane();
     c.add(new movingDisk());
-
     w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     w.setResizable(false);
     w.setVisible(true);
   }
 }
-
